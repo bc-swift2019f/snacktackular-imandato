0a5f9e61d7f1d909b3700a814830627f29ccb644
Ivelisse Mandato <mandatoi@bc.edu>
2019-11-03 20:48:07 -0500

    6.5 completed

--- Snacktacular/Spot.swift
+++ Snacktacular/Spot.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import CoreLocation
+import Firebase
 
 class Spot {
     var name: String
@@ -17,6 +18,18 @@ class Spot {
     var numberOfReviews: Int
     var postingUserID: String
     var documentID: String
+    
+    var longitude: CLLocationDegrees {
+        return coordinate.longitude
+    }
+    
+    var latitude: CLLocationDegrees {
+        return coordinate.latitude
+    }
+    
+    var dictionary: [String: Any] {
+        return ["name": name, "address": address, "longitude": longitude, "latitude": latitude, "averageRating": averageRating, "numberOfReviews": numberOfReviews, "postingUserID": postingUserID]
+    }
     
     init(name: String, address: String, coordinate: CLLocationCoordinate2D, averageRating: Double, numberOfReviews: Int, postingUserID: String, documentID: String)  {
         self.name = name
@@ -30,5 +43,41 @@ class Spot {
     
     convenience init () {
         self.init(name: "", address: "", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: "")
+    }
+    
+    func saveData(completion: @escaping (Bool) -> ()) {
+        let db = Firestore.firestore()
+        // Grab the userID
+        guard let postingUserID = (Auth.auth().currentUser?.uid) else {
+            print("*** ERROR: Could not save data becuase we don't have a valid postingUserID")
+            return completion (false)
+        }
+        self.postingUserID = postingUserID
+        // Create the dictionary representing the data we want to save
+        let dataToSave = self.dictionary
+        // if we HAVE saved a record, we'll have a documentID
+        if self.documentID != "" {
+            let ref = db.collection("spots").document(self.documentID)
+            ref.setData(dataToSave) { (error) in
+                if let error = error {
+                    print("*** ERROR: updating document \(self.documentID) \(error.localizedDescription)")
+                    completion(false)
+                } else {
+                    print("^^^ Document updated with ref ID \(ref.documentID)")
+                    completion (true)
+                }
+            }
+        } else {
+            var ref: DocumentReference? = nil //Let firestore create the new document ID
+            ref = db.collection("spots").addDocument(data: dataToSave) { error in
+                if let error = error {
+                    print("*** ERROR: creating new document \(self.documentID) \(error.localizedDescription)")
+                    completion(false)
+                } else {
+                    print("^^^ new document created with ref ID \(ref?.documentID ?? "unknown")")
+                    completion (true)
+                }
+            }
+        }
     }
 }
