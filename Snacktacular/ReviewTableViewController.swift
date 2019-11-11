129a0aaf53bd39022f2e73844c8eff6d31a82553
Ivelisse Mandato <mandatoi@bc.edu>
2019-11-10 21:57:30 -0500

    9.12 completed

---
+++ Snacktacular/ReviewTableViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ReviewTableViewController.swift
+//  Snacktacular
+//
+//  Created by Ivelisse Mandato on 11/10/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import UIKit
+
+class ReviewTableViewController: UITableViewController {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var postedByLabel: UILabel!
+    @IBOutlet weak var reviewTitle: UITextField!
+    @IBOutlet weak var reviewDateLabel: UILabel!
+    @IBOutlet weak var reviewView: UITextView!
+    @IBOutlet weak var cancelBarButton: UIBarButtonItem!
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var deleteBarButton: UIButton!
+    @IBOutlet weak var buttonsBackgroundView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // hide keyboard if we tap outside of a field
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
+
+    }
+    
+    func leaveViewController() {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    @IBAction func reviewTitleChanged(_ sender: UITextField) {
+    }
+    
+
+    @IBAction func returnTitleDonePressed(_ sender: UITextField) {
+    }
+    
+    @IBAction func deleteButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        leaveViewController()
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+
+}
